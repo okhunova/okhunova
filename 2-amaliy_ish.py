e506f4f903222bb6c94ece62b68a3940fcf90642
okhunova <114384810+okhunova@users.noreply.github.com>
2022-10-26 08:38:46 +0500

    Add files via upload

---
+++ 2-amaliy_ish.py
@@ -0,0 +1,5 @@
+r=int(input("r="))
+pi=3.14
+L=2*pi*r
+S=pi*r**2
+print(f"Uzunligi {L} ga teng yuzasi {S} ga teng")
---
+++ 3-amaliy_ish.py
@@ -0,0 +1,6 @@
+a=int(input("a="))
+b=int(input("b="))
+if (a%2!=0 or b%2!=0):
+    print("juft son bor")
+else:
+    print("juft son yoq")
